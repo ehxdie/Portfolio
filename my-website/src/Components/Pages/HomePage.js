d40ffc7365ab3b030c78c82ff3fad5000a66f8c2
Edidiong <ephraimedy@gmail.com>
2023-12-18 18:30:57 +0100

    Added hover css styles

--- my-website/src/Components/Pages/HomePage.js
+++ my-website/src/Components/Pages/HomePage.js
@@ -2,31 +2,41 @@ import React, { useState } from 'react'
 import Footer from '../Footer'
 import Header from '../Header'
 import "../../index.css";
-import SouthOutlinedIcon from '@mui/icons-material/SouthOutlined';
 import Projects from '../Projects';
-
+import SouthOutlinedIcon from '@mui/icons-material/SouthOutlined';
 
 
 function HomePage() {
   // Setting up dummy data for the projects data
   const [projects, setprojects] = useState([
   {
-    number: 1,
-    name: "Project name",
+    name: "Mern Application",
     description: "Project description",
-    stack: "stack description",
+    stack: "Stack description",
     link: "http://stackoverflow.com",
     img: "img",
   },
   {
-    number: 2,
-    name: "Project name",
+    name: "Electronic Voting System",
     description: "Project description",
-    stack: "stack description",
+    stack: "Stack description",
     link: "http://stackoverflow.com",
     img: "img",
   
-  }])
+    }, {
+      name: "Chat App",
+      description: "Project description",
+      stack: "Stack description",
+      link: "http://stackoverflow.com",
+      img: "img",
+    }, 
+      {
+      name: "OOP da game",
+      description: "Project description",
+      stack: "Stack description",
+      link: "http://stackoverflow.com",
+      img: "img",
+    }])
 
   const ProjectCard = projects.map(project => {
     return (
@@ -51,14 +61,18 @@ function HomePage() {
                 <div className='Homepage-header-section'>
                   <p className='Homepage-header-text1'>Hello, I'm <br></br> Edidiong Ephraim.</p>
                   <div className='icon-area'>
-                    <SouthOutlinedIcon className='icon'/>
-                    <p className='icon-area-text'>Download my cv?</p>
+                    <p className='icon-area-text'>Download my CV?</p>
                   </div>
                 </div>
                 <p className='Homepage-header-text2'>I'm a Full Stack and Web 3 Developer.</p>
 
             </div>
             <div className='Homepage-body'>
+              <div className='projects-headline'>
+                  Some of the things I've built...
+                <SouthOutlinedIcon className='icon' />
+              </div>
+
               {ProjectCard}
             </div>
         </div>
--- my-website/src/Components/Projects.js
+++ my-website/src/Components/Projects.js
@@ -1,18 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
+import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
 
 function Projects(props) {
+  const [infocontrol, setinfocontrol] = useState(true);
+
+  const handleclick = () => setinfocontrol(!infocontrol);
+
+  const propsDescription = <p>Project description <KeyboardArrowDownOutlinedIcon/></p>;
+  console.log(infocontrol);
+
   return (
+    <>
     <div className='projects-container'>
-        <div className='projects-container-text'>
-              <p className='projects-container-text1'>{props.number} {props.name}</p>
-              <p className='projects-container-text2'>{props.description}</p>
-              <p className='projects-container-text3'>{props.stack}</p>
-            <a href={props.link} target='_blank' rel='noopener noreferrer'>{props.link}</a>
+        
+         <div className='projects-container-text'>
+              <p className='projects-container-text1'>Title: {props.name}</p>
+              <div className='projects-container-text2-header' onClick={handleclick}>
+                 <div className='text2-header'> Project description {infocontrol ? <KeyboardArrowDownOutlinedIcon /> : <KeyboardArrowUpOutlinedIcon />}</div>
+                 {infocontrol ? null : <p className='projects-container-text2'>{props.description}</p>}
+              </div>
+          <div className='projects-container-text3-header' onClick={handleclick}P>
+                <div className='text2-header'> Tech Stack {infocontrol ? <KeyboardArrowDownOutlinedIcon /> : <KeyboardArrowUpOutlinedIcon />}</div>
+                {infocontrol ? null : <p className='projects-container-text3'>{props.stack}</p>}
+            </div> 
+
+            <a className='projects-container-link'href={props.link} target='_blank' rel='noopener noreferrer'>{props.link}</a>
+        
         </div>
+
         <div className='projects-container-image'>
             <img src={props.img} className='img'/>
         </div>
     </div>
+    </>
   )
 }
 
